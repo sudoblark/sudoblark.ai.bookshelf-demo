4dee0261c7450307faf580485e57b34c91ad19e5
Benjamin Clark <bclark@sudoblark.com>
2026-04-21 08:58:41 +0100

    fix(infra) circular dependencies

--- infrastructure/aws-sudoblark-development/lambda.tf
+++ infrastructure/aws-sudoblark-development/lambda.tf
@@ -1,7 +1,17 @@
-module "lambda" {
-  source     = "../../modules/infrastructure/lambda"
-  lambdas    = module.data.lambdas
-  layer_arns = module.lambda_layer.layer_arns
+locals {
+  # Resolve layer_names to ARNs at the deployment layer, where runtime ARNs are available.
+  # The data module declares layer_names by short name; here we look them up and merge into layers.
+  lambdas_with_resolved_layers = [
+    for lambda in module.data.lambdas : merge(lambda, {
+      layers = concat(
+        lambda.layers,
+        [for name in lambda.layer_names : module.lambda_layer.layer_arns[name]]
+      )
+    })
+  ]
+}
 
-  depends_on = [module.lambda_layer]
+module "lambda" {
+  source  = "../../modules/infrastructure/lambda"
+  lambdas = local.lambdas_with_resolved_layers
 }
--- infrastructure/aws-sudoblark-development/step_functions.tf
+++ infrastructure/aws-sudoblark-development/step_functions.tf
@@ -1,6 +1,4 @@
 module "step_functions" {
   source         = "../../modules/infrastructure/step_functions"
   state_machines = module.data.step_functions
-
-  depends_on = [module.lambda]
 }
